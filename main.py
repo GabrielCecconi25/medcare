d5b5f307a9031ddb6d2dea4ba97fc2f94b4a5b97
Gabriel Torres Cecconi <2303789@SALAS.AULAS>
2024-10-31 20:35:02 -0300

    Starting POO Classes in project and starting system logic!

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-Pessoa {
-    nome
-    cpf
-    rg
-    convenio
-}
+import Atendimento
 
-cadastroConvenio {
-    empresa
-    numero
-    nivel
-}
+def main():
+    cod = int(input("1. Paciente\n2. Medico\n3. Fila\n4. Sala\nEscolher opção inicial: "))
 
+main()
